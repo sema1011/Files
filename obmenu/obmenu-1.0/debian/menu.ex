6da86019539f936ba68bbe8f134302639bc91764
Sema1011 <sema19689@gmail.com>
2013-12-05 22:18:53 +0400

    update

--- obmenu/obmenu-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(obmenu):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="obmenu" command="/usr/bin/obmenu"
--- obmenu/obmenu-1.0/debian/obmenu.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the obmenu package
-#
-0 4	* * *	root	[ -x /usr/bin/obmenu_maintenance ] && /usr/bin/obmenu_maintenance
